96b40a323dcbc152de43a50fe1fcccc12a236a8a
shubham kumar <shubhamfarainzi@gmail.com>
2026-02-18 17:30:34 +0530

    added new

---
+++ git&github/gitNotes.js
@@ -0,0 +1,27 @@
+// Git--> git is a tool version control system that helps you save , track and manage changes in  your code or file
+
+//In easy language --> git is like a time machine for your project it remembers every changes you make so you can go back to older version, fix mistakes and work with others salfely.
+
+// Git commands --> 
+// 1) create a folder --> mkdir folderName
+// 2) clean all code in terminal --> clear or cls
+// 3) create a new file --> touch fileName or echo>fileName
+// 4)create a git repository --> git init
+// 5) add a file in stagging area --> git add fileName
+// 6)add all files in stagging area --> git add .
+// 7)saving your project(files) permanently with a message -->git commit -m "your message"
+
+//8) git log --> it will show you all commits
+//9) git status --> show you which files are untracked , which file are in stagging area , which files need to be commited
+
+
+// github commands --> 
+// 1) create a account on github
+// 2) git config --global user.name "your github name"
+// 3) git config --global user.email " your github email"
+// 4) check your github name --> git config user.name
+// 5) check your github email --> git config user.email
+// 6) rename your current branch -->git branch -M newName
+//7) connect your localsystem git repository to online git repository git remote add origin "github repository url"
+//8)push your code to online main branch--> git push origin main
+
