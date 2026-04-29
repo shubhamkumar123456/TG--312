699b05b86120f996dfbc00003863eaf4fd09746b
shubham kumar <shubhamfarainzi@gmail.com>
2026-04-29 16:37:10 +0530

    added files

--- socialMediaApp/backend/config/db.js
+++ socialMediaApp/backend/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 async function mongodbConnection(){
    try {
-    await mongoose.connect('mongodb://localhost:27017/project1');
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@socialmediag6.2h1hdjm.mongodb.net/?appName=SocialMediaG6`);
+
     console.log("mongodb connected successfully")
    } catch (error) {
         console.log("error in connecting Db")
